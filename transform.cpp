ba0ac0e5e075e01aaa6cf49c537a9b71bf179bc3
val <val@ubuntu.ubuntu-domain>
2014-03-18 21:10:40 +0100

    Committer: val <val@ubuntu.ubuntu-domain>  Changes to be committed: 	modified:   transform.cpp
     ajout d'une premiere version de la fonction convert to int pour transformer au format String<double>

--- transform.cpp
+++ transform.cpp
@@ -50,31 +50,60 @@ int transformSequence(bool *pattern,int patternSize,int patternFinalSize, String
   return i;
 }
 
+int transformSequenceToDouble(bool *pattern,int patternSize,int patternFinalSize, String<Dna5> sequence,String<double> *transformee){
+  unsigned i,size;
 
-/* exemple d'util */
-/*
-  int main(int, char const **)
-  {
-  bool *pattern;
-  int patternLength;
-
-  // Build reads and genomes
-  String<Dna> genome = "ATTAATTAATTAATTAATTAATTAATTA";
-    
-  String<Dna> transformee;
-    
-  pattern=(bool *)malloc(4*sizeof(bool));
-
-  patternLength=convertPattern("_##_",pattern);
-    
-  transformSequence(pattern,genome,transformee);
-
-  std::cout << genome << std::endl;
-
-    
-  std::cout << transformee << std::endl;  
-    
-  return 1;
+  if(patternFinalSize>21){
+    std::cout<<"impossible d'encoder le resultat sur un double"<<std::endl;
+    return -1;
   }
+  else{
+    size=length(sequence);  
+    reserve(transformee,patternFinalSize*size);
+    int offset=0;
+    double tmp=0;
 
-*/
+    for(i = 0; i < size; ++i){
+      if(pattern[i%patternSize]){
+	tmp+=(ordValue(sequence[i%size]) * (3*offset) ) ;
+	offset++;
+	if(offset==patternFinalSize){
+	  offset=0;
+	  appendValue(*transformee,tmp);
+	}
+      }
+    }
+
+    std::cout << "taille tranformee 1er passage : " << length(*transformee) << std::endl; 
+    while(pgcd((unsigned int)patternSize,(unsigned int)i%patternSize)!=1){
+      sequence+='N';
+      size++;
+      if(pattern[i%patternSize]){
+	tmp+=(ordValue(sequence[i%size]) * (3*offset) ) ;
+	offset++;
+	if(offset==patternFinalSize){
+	  offset=0;
+	  appendValue(*transformee,tmp);
+	}
+      }
+      i++;
+    }
+    std::cout << "taille transformee avec ajout N : " << length(*transformee) << std::endl; 
+    size=length(sequence);
+    reserve(transformee,patternFinalSize*size);
+
+    for(; i < size*patternFinalSize; ++i){
+      if(pattern[i%patternSize]){
+	tmp+=(ordValue(sequence[i%size]) * (3*offset) ) ;
+	offset++;
+	if(offset==patternFinalSize){
+	  offset=0;
+	  appendValue(*transformee,tmp);
+	}
+      }
+    } 
+    std::cout << "taille finale transformee : " << length(*transformee) << std::endl; 
+    return i;
+  }
+}
+
